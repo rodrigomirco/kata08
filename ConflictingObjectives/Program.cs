5cf0bd83a7cbd64eb7ea3d11a78c4f45a591f525
RodrigoLMcGill <rodrigo.lisboamirco@mail.mcgill.ca>
2021-09-23 10:46:43 -0400

    Initial commit

---
+++ ConflictingObjectives/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConflictingObjectives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
